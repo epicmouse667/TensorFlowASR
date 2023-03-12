f035b5fa055322745b32ae4208c23f328977075e
Wzx <2544547787@qq.com>
2023-03-12 11:35:29 +0800

    Revert "undo changes in extract func in text_featurizer"
    This reverts commit 3bd38fa9cf725e82ef6e7e790672d3ea106330b4.

--- tensorflow_asr/featurizers/text_featurizers.py
+++ tensorflow_asr/featurizers/text_featurizers.py
@@ -198,7 +198,8 @@ class CharFeaturizer(TextFeaturizer):
             sequence of ints in tf.Tensor
         """
         text = self.preprocess_text(text)
-        text = list(text.strip()) # remove trailing space
+        text = text.strip() # remove trailing space
+        text = text.split()
         indices = [self.tokens2indices[token] for token in text]
         return tf.convert_to_tensor(indices, dtype=tf.int32)
 
